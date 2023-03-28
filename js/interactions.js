80f2f460c8f4ee07aa092beda9a1cc4736ba1155
susunookes <susunookes@gmail.com>
2023-03-29 00:59:25 +0900

    カーソルの動きに同期するアニメーション追加

--- js/interactions.js
+++ js/interactions.js
@@ -9,3 +9,15 @@ LottieInteractivity.create({
         },
       ],
   });
+
+  LottieInteractivity.create({
+    player: "#blueprint-lottie",
+    mode:"cursor",
+    actions: [
+        {
+            position: { x: [0, 1], y: [0, 1] },
+            type: "seek",
+            frames: [0, 42]
+        }
+    ]
+});
